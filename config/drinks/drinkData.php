256d9e29d62995b487045798147f0383b41bee41
Kim Tran <lucian457fcde@gmail.com>
2025-04-05 22:12:58 +0700

    change image location

--- config/drinks/drinkData.php
+++ config/drinks/drinkData.php
@@ -4,22 +4,22 @@ return [
         [
             'name'=> 'Coffee',
             'ingredients'=> ['Coffee','Sugar'],
-            'imagePath'=> 'storage/app/public/images/coffee.webp',
+            'imagePath'=> 'public/images/coffee.webp',
         ],
         [
             'name'=> 'Milk Coffee',
             'ingredients'=> ['Coffee','Milk'],
-            'imagePath'=> 'storage/app/public/images/milkcoffee.webp',
+            'imagePath'=> 'public/images/milkcoffee.webp',
         ],
         [
             'name'=> 'Tea',
             'ingredients'=> ['Tea','Sugar'],
-            'imagePath'=> 'storage/app/public/images/icetea.webp',
+            'imagePath'=> 'public/images/icetea.webp',
         ],
         [
             'name'=> 'Milk Tea',
             'ingredients'=> ['Tea','Milk'],
-            'imagePath'=> 'storage/app/public/images/milktea.png',
+            'imagePath'=> 'public/images/milktea.png',
         ]
     ]
 ];
