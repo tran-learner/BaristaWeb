fa8b3f54bae4b85d500e692ac3b8ccf8dbc95811
Your Name <you@example.com>
2025-04-24 10:08:36 +0700

    a

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,32 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use PayOS\PayOS;
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
+
+    protected PayOS $payOS;
+
+    public function __construct()
+    {
+        $this->payOS = new PayOS(
+            env("PAYOS_CLIENT_ID"),
+            env("PAYOS_API_KEY"),
+            env("PAYOS_CHECKSUM_KEY")
+        );
+    }
+
+    protected function handleException(\Throwable $th)
+    {
+        return response()->json([
+            "error" => $th->getCode(),
+            "message" => $th->getMessage(),
+            "data" => null
+        ]);
+    }
 }
