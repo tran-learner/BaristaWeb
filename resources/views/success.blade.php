ecf659be200e08b71a65906d7f6ab6a0abbe89e5
Your Name <you@example.com>
2025-04-25 22:17:35 +0700

    a

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -16,9 +16,32 @@
                 <a href="mailto:support@payos.vn">support@payos.vn</a>
             </p>
             <a href="/" id="return-page-btn"
-                >Trở về trang Tạo Link thanh toán</a
-            >
-            <p>{{ json_encode($paymentData) }}</p>
+                >Trở về trang Tạo Link thanh toán</a>
+            <p id = "drink-data">{{ json_encode($paymentData) }}</p>
         </div>
     </body>
 </html>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        pElement = document.getElementById('drink-data')
+        drinkData = pElement.textContent
+        console.log(drinkData)
+        async function sendIngredientQuantity() {
+            // Create payload from input values
+            try {
+                const response = await fetch('https://67d1-2402-800-63b5-b58e-24a2-653-171e-715f.ngrok-free.app/pumphandle', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: drinkData;
+                });
+
+                response.ok ? alert('Barista completed') : alert('Barista request receive error :((');
+            } catch (error) {
+                console.error('Fetch error:', error);
+            }
+        }
+    });
+</script>
