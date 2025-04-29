d44f1a79c2f1d5163d870b7f6c23a21e4cc27981
Kim Tran <lucian457fcde@gmail.com>
2025-04-29 22:41:53 +0700

    fix-bg

--- resources/views/drinkList.blade.php
+++ resources/views/drinkList.blade.php
@@ -6,7 +6,7 @@
         class="flex flex-wrap gap-4 justify-between sm:justify-between w-5/6 max-w-[360px] sm:max-w-full">
         @foreach ($drinks as $drink)
             <a href="{{ route('getIngredients', ['drink' => $drink['name']]) }}" id="drinkSelector"
-                class="flex flex-col py-3 my-3 rounded-2xl shadow-md bg-white bg-black text-gray-800 w-[160px] sm:w-[210px] hover:shadow-lg transition">
+                class="flex flex-col py-3 my-3 rounded-2xl shadow-md bg-white text-gray-800 w-[160px] sm:w-[210px] hover:shadow-lg transition">
 
                 <div id="imgContainer" class="flex-4">
                     {{-- <img src="{{ Vite::asset($drink['imagePath']) }}" alt=""
