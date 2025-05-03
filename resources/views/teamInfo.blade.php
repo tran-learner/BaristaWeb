9c5c4d0117c232278560bb97b92223ec9fa4b30a
Your Name <you@example.com>
2025-05-03 17:07:03 +0700

    a

--- resources/views/teamInfo.blade.php
+++ resources/views/teamInfo.blade.php
@@ -5,21 +5,22 @@
     <div id="drinkListContainer"
         class="flex flex-wrap gap-4 justify-between sm:justify-between w-5/6 max-w-[360px] sm:max-w-full">
         @foreach ($infos as $info)
-            <a href="{{ route('getInfos', ['info' => $info['name']]) }}" id="infoSelector"
-                class="flex flex-col py-3 my-3 rounded-2xl shadow-md bg-white text-gray-800 w-[160px] sm:w-[210px] hover:shadow-lg transition opacity-clicked">
+            <div id="infoSelector"
+                class="flex flex-col py-3 my-3 rounded-2xl shadow-md bg-white text-gray-800 w-[160px] sm:w-[210px] transition">
 
-                <div id="imgContainer" class="flex-4">
-                    {{-- <img src="{{ Vite::asset($drink['imagePath']) }}" alt=""
-                            class="w-[160px] h-[150px] sm:w-[210px] sm:h-[180px]"> --}}
+                <a href="{{ route('getInfos', ['info' => $info['name']]) }}" class="flex flex-col items-center opacity-clicked">
                     <img src="{{ asset($info['imagePath']) }}" alt=""
-                        class="w-[150px] h-[130px] sm:w-[210px] sm:h-[180px]">
-                    <link rel="stylesheet" href="{{ asset('css/opacity_bt.css') }}">
+                        class="w-[150px] h-[130px] sm:w-[210px] sm:h-[180px] rounded-t-2xl">
+                    <h1 id="drinkName" class="font-bold text-center text-gray-700 py-2">{{ $info['name'] }}</h1>
+                </a>
+                <div class="flex justify-around items-center py-2 ">
+                    <a href="https://www.facebook.com/{{ $info['facebook'] }}" target="blank"><img
+                            src="{{ asset('images/app_icon/facebook.png') }}" alt="Facebook" class="w-[30px] h-[30px] opacity-clicked"></a>
+                    <a href="mailto:{{ $info['email'] }}"><img src="{{ asset('images/app_icon/email.png') }}"
+                            alt="Email" class="w-[30px] h-[30px] opacity-clicked"></a>
                 </div>
-                <div class="flex-1">
-                    <h1 id="drinkName" class="font-bold text-center text-gray-700">{{ $info['name'] }}<br><a href="{{ asset('https://www.facebook.com/' .$info['facebook']) }}" target="blank"><img src="images/app_icon/facebook.png" alt="Facebook" class="w-[35px]"></a>
-                        <a href="{{ asset('mailto:' .$info['email']) }}" ><img src="images/app_icon/email.png" alt="Email" class="w-[35px]"></a></h1>
-                </div>
-            </a>
+
+            </div>
         @endforeach
     </div>
 </div>
